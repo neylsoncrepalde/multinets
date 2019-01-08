a7037476bb18ebc8d64983094150d43ff051fba2
Neylson Crepalde <neylsoncrepalde@gmail.com>
2019-01-08 18:17:13 -0200

    make less space between levels

--- src/open_plot.cpp
+++ src/open_plot.cpp
@@ -20,11 +20,11 @@ NumericMatrix openPlot(NumericMatrix A) {
   for (int i = 0; i < n; i++){
     if (A(i,1) > 0){
       B(i,0) = A(i,0);
-      B(i,1) = A(i,1) + 0.3;
+      B(i,1) = A(i,1) + 0.25;
     }
     if (A(i,1) < 0){
       B(i,0) = A(i,0);
-      B(i,1) = A(i,1) - 0.3;
+      B(i,1) = A(i,1) - 0.25;
     }
   }
   
